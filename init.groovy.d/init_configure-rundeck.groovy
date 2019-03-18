56e09af6df4c3feb9f90c5785aaebf77eb933fc5
kamiNaranjo <cami.tsuki@gmail.com>
2019-03-18 17:17:50 -0500

    Prueba 2 de rundeck

--- init.groovy.d/init_configure-rundeck.groovy
+++ init.groovy.d/init_configure-rundeck.groovy
@@ -1,12 +1,12 @@
 import org.rundeck.api.RundeckClient;
+import jenkins.model.Jenkins;
 
 def name = "name2"
 def url  = "http://url.com"
 def aut  = "holiholi"
 def api  = 12
 
-Jenkins jenkins = Jenkins.getInstance()
-rundeck = jenkins.instance.getExtensionList(org.jenkinsci.plugins.rundeck.RundeckNotifier.RundeckDescriptor.class)[0];
+rundeck = Jenkins.instance.getExtensionList(org.jenkinsci.plugins.rundeck.RundeckNotifier.RundeckDescriptor.class)[0];
 println "Inicio configuracion rundeck"
 RundeckClient rundeckClient = RundeckClient.builder()
 								.url(url)
