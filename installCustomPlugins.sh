697a9b4eaf2204dc7c23175f747f360a95285d8f
haguerrero29 <32080809+haguerrero29@users.noreply.github.com>
2019-03-29 13:59:13 -0500

    se modifica path donde quedaran los plugins en el pod

--- installCustomPlugins.sh
+++ installCustomPlugins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PLUGINS_HOME=/tmp
+PLUGINS_HOME=/var/jenkins_home/plugins
 ARTIFACTORY_URL=https://artifactory-labo.suramericana.com.co/artifactory/jenkinsCustomPlugins
 filename=$1
 IFS=$'\n'
 for next in $(cat $filename); do
     curl $ARTIFACTORY_URL/$next/$next.hpi -o $PLUGINS_HOME/$next.jpi
-    unzip $ARTIFACTORY_URL/$next.jpi -d $ARTIFACTORY_URL/$next
+    #unzip $ARTIFACTORY_URL/$next.jpi -d $ARTIFACTORY_URL/$next
     echo "$next read from $filename"
 done
 exit 0
